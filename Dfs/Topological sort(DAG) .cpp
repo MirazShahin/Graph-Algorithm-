24a13718a5ab45b5a7a46a2a0b28438d11d4d916
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-04-07 16:28:34 +0600

    Update Topological sort(DAG) .cpp

--- Dfs/Topological sort(DAG) .cpp
+++ Dfs/Topological sort(DAG) .cpp
@@ -4,13 +4,12 @@ using namespace std;
 vector<vector<int>> adj;
 const int N = 1e6 + 5;
 int vis[N];
-stack<int> s;
 
-void topological_sort_dfs(int node) {
+void topological_sort_using_dfs(int node, stack<int> &s) {
     vis[node] = 1;
     for(auto child : adj[node]) {
         if(vis[child] == 1) continue;
-        topological_sort_dfs(child);
+        topological_sort_using_dfs(child, s);
     }
     s.push(node);
 }
@@ -23,10 +22,10 @@ int main() {
         cin >> u >> v;
         adj[u].push_back(v);
     }
-
+    stack<int> s;
     for(int i = 0; i < node; i++ ) {
         if(!vis[i]) {
-            topological_sort_dfs(i);
+            topological_sort_using_dfs(i + 1, s);
         }
     }
     vector<int> ans;
@@ -37,6 +36,6 @@ int main() {
     }
     for(auto it : ans ) cout << it << " ";
     cout << endl;
-    
+
     return 0;
 }
